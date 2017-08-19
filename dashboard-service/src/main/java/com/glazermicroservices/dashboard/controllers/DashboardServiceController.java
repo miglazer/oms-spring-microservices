84fae0d9201ddbc26f6710dd19e6729cb9f3b6f9
Michael Glazer <miglazer@gmail.com>
2017-08-19 14:40:45 -0400

    Initial commit

---
+++ dashboard-service/src/main/java/com/glazermicroservices/dashboard/controllers/DashboardServiceController.java
@@ -0,0 +1,26 @@
+package src.main.java.com.glazermicroservices.dashboard.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import src.main.java.com.glazermicroservices.dashboard.model.Account;
+
+/**
+ * Created by michaelglazer on 8/19/17.
+ */
+@RestController
+@RequestMapping(value="/v1/accounts/{accountId}/dashboardcomponents")
+public class DashboardServiceController {
+
+    @RequestMapping(value = "/{accountId}", method = RequestMethod.GET)
+    public Account getAccounts(
+            @PathVariable("accountId") String id) {
+
+        return new Account();
+
+    }
+
+
+
+}
